8ae40c5ecfe6d7432ea561eb1ebb0cd880db6615
David Mann <david@ultramusician.com>
2020-09-01 10:56:13 -0700

    Initial Commit

---
+++ AudioFramework/AudioFramework.h
@@ -0,0 +1,19 @@
+//
+//  AudioFramework.h
+//  AudioFramework
+//
+//  Created by Admin on 9/1/20.
+//  Copyright © 2020 UltraMusician. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioFramework.
+FOUNDATION_EXPORT double AudioFrameworkVersionNumber;
+
+//! Project version string for AudioFramework.
+FOUNDATION_EXPORT const unsigned char AudioFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioFramework/PublicHeader.h>
+
+
